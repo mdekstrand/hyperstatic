f2b519416bedbef05561e15e663c0b3352eccbfd
Michael Ekstrand <md@ekstrandom.net>
2024-01-14 11:01:56 -0500

    support jsx-runtime in the browser

---
+++ browser/jsx-runtime.ts
@@ -0,0 +1,11 @@
+import { h } from "../hyperstatic.ts";
+
+export const jsx = h.jsx;
+export const jsxs = h.jsxs;
+export const Fragment = h.Fragment;
+
+export namespace JSX {
+  export interface IntrinsicElements {
+    [name: string]: any;
+  }
+}
