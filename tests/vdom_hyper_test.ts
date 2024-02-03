c9ae6789a0f175b42577fc5c5ee937d0cf787336
Michael Ekstrand <md@ekstrandom.net>
2024-02-03 09:03:56 -0500

    VDOM tests that don't work yet

---
+++ tests/vdom_hyper_test.ts
@@ -0,0 +1,76 @@
+import { assertEquals } from "std/assert/mod.ts";
+import { beforeEach, describe, it } from "std/testing/bdd.ts";
+import { Document, Element, Node } from "deno-dom";
+
+import { HyperStatic } from "../core/defs.ts";
+import { hyperstatic } from "../core/hyper.ts";
+import { VDElement, VDNode, VirtualContext } from "../virtual/mod.ts";
+import { renderDOM } from "../virtual/mod.ts";
+
+describe("h", () => {
+  let h: HyperStatic<VDNode, VDElement>;
+  let doc: Document;
+  beforeEach(() => {
+    let ctx = new VirtualContext();
+    h = hyperstatic(ctx);
+    doc = new Document();
+  });
+
+  it("creates an empty element", () => {
+    let elt = renderDOM(h("div"), doc);
+    assertEquals(elt.nodeType, Node.ELEMENT_NODE);
+    assertEquals(elt.nodeName, "DIV");
+    assertEquals(elt.childNodes.length, 0);
+  });
+
+  it("ignores null", () => {
+    let elt = renderDOM(h("div", {}, null), doc);
+    assertEquals(elt.nodeType, Node.ELEMENT_NODE);
+    assertEquals(elt.nodeName, "DIV");
+    assertEquals(elt.childNodes.length, 0);
+  });
+
+  it("creates an element with attributes", () => {
+    let elt = renderDOM(h("a", { id: "link", href: "https://example.com" }), doc);
+    assertEquals(elt.nodeType, Node.ELEMENT_NODE);
+    assertEquals(elt.nodeName, "A");
+    assertEquals(elt.childNodes.length, 0);
+    assertEquals(elt.id, "link");
+    assertEquals(elt.getAttribute("href"), "https://example.com");
+  });
+
+  it("creates an element with an ID and link", () => {
+    let elt = renderDOM(h("a#link", { href: "https://example.com" }), doc);
+    assertEquals(elt.nodeType, Node.ELEMENT_NODE);
+    assertEquals(elt.nodeName, "A");
+    assertEquals(elt.childNodes.length, 0);
+    assertEquals(elt.id, "link");
+    assertEquals(elt.getAttribute("href"), "https://example.com");
+  });
+
+  it("creates an element with a text child", () => {
+    let elt = renderDOM(h("span", "fish"), doc);
+    assertEquals(elt.nodeType, Node.ELEMENT_NODE);
+    assertEquals(elt.nodeName, "SPAN");
+    // the text is right
+    assertEquals(elt.textContent, "fish");
+    // there aren't any child elements
+    assertEquals(elt.childElementCount, 0);
+  });
+
+  it("creates nested with classes", () => {
+    let elt = renderDOM(
+      h("nav.top", [
+        h("ul", [
+          h("li", h("a", { href: "/" }, "Home")),
+          h("li", h("a", { href: "/search" }, "Search")),
+        ]),
+      ]),
+      doc,
+    );
+    assertEquals(elt.nodeName, "NAV");
+    assertEquals(elt.classList[0], "top");
+    assertEquals(elt.children[0].nodeName, "UL");
+    assertEquals(elt.children[0].childElementCount, 2);
+  });
+});
--- virtual/mod.ts
+++ virtual/mod.ts
@@ -2,3 +2,6 @@
  * Hyperstatic with a virtual DOM.  Much like Hastscript, but interoperates with
  * DOM trees.
  */
+export { VirtualContext } from "./context.ts";
+export type { VDElement, VDNode } from "./vdom.ts";
+export { render as renderDOM } from "./render.ts";
