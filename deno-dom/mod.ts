3b33b4ed1d84a3c4ea76856f39ebaf20e29d0596
Michael Ekstrand <md@ekstrandom.net>
2024-02-03 18:47:46 -0500

    dom types

--- deno-dom/mod.ts
+++ deno-dom/mod.ts
@@ -1,8 +1,12 @@
 import { Document, DocumentFragment, Element, Node } from "deno-dom";
+import { HyperStatic } from "../core/defs.ts";
 import { hyperstatic } from "../core/hyper.ts";
 import { DOMContext } from "../core/dom.ts";
 
 export { Document, DocumentFragment, Element, Node };
+
+export type DDHyperStatic = HyperStatic<Node, Element>;
+export type DDContext = DOMContext<Node, Element, Document>;
 
 /**
  * A {@link hyperstatic} instance using DenoDOM.
