40d98c83162d68e1320370d1831f0e84fd846e85
Michael Ekstrand <md@ekstrandom.net>
2024-04-15 12:01:32 -0400

    fix typing of browser/mod

--- browser/mod.ts
+++ browser/mod.ts
@@ -6,7 +6,9 @@ import { HyperStatic, hyperstatic } from "../core/hyper.ts";
  * Default HyperStatic implementation using the document from the global `window` object.
  * @type {HyperStatic}
  */
-export const h: HyperStatic<Node, Element> = hyperstatic(new DOMContext(window.document));
+export const h: HyperStatic<Node, Element> = hyperstatic<Node, Element>(
+  new DOMContext(window.document),
+);
 export default h;
 export const createElement = h.createElement;
 export const Fragment = h.Fragment;
