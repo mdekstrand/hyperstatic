6ec4d8dddd5cc7e639ea27af1cb0f9708f9543e1
Michael Ekstrand <mdekstrand@drexel.edu>
2025-05-21 13:54:38 -0400

    fix browser

--- browser/mod.ts
+++ browser/mod.ts
@@ -1,12 +1,12 @@
 // deno-lint-ignore-file no-window
 import { DOMContext } from "../core/dom.ts";
-import { HyperStatic, hyperstatic } from "../core/hyper.ts";
+import { HyperStatic, makeHyper } from "../core/hyper.ts";
 
 /**
  * Default HyperStatic implementation using the document from the global `window` object.
  * @type {HyperStatic}
  */
-export const h: HyperStatic<Node, Element> = hyperstatic<Node, Element>(
+export const h: HyperStatic<Node, Element> = makeHyper<Node, Element>(
   new DOMContext(window.document),
 );
 export default h;
