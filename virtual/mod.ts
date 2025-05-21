623e9883a285d90584e74621db6081fd8dfccde7
Michael Ekstrand <mdekstrand@drexel.edu>
2025-05-21 13:56:22 -0400

    explicit vdom

--- virtual/mod.ts
+++ virtual/mod.ts
@@ -7,8 +7,10 @@ export { VirtualContext };
 export type { VDElement, VDNode } from "./vdom.ts";
 export { render as renderDOM } from "./render.ts";
 import { makeHyper } from "../core/hyper.ts";
+import { HyperStatic } from "../core/defs.ts";
+import { VDElement, VDNode } from "./vdom.ts";
 
 /**
  * A {@link hyperstatic} using the virtual DOM.
  */
-export const h = makeHyper(new VirtualContext());
+export const h: HyperStatic<VDNode, VDElement> = makeHyper(new VirtualContext());
