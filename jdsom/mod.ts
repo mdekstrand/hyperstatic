d7a3d8ccb283f4b64192c98ec138e7d4f5f75746
Michael Ekstrand <md@ekstrandom.net>
2025-07-14 21:10:38 -0400

    jsdom

--- jdsom/mod.ts
+++ jdsom/mod.ts
@@ -1,16 +1,17 @@
-import { Document, DocumentFragment, Element, Node } from "jsdom";
+/// <reference lib="dom" />
+import { JSDOM } from "jsdom";
 import { HyperStatic } from "../core/defs.js";
 import { DOMContext } from "../core/dom.js";
 import { makeHyper } from "../core/hyper.js";
 
-export { Document, DocumentFragment, Element, Node };
+// export { Document, DocumentFragment, Element, Node };
 
-export type DDHyperStatic = HyperStatic<Node, Element>;
-export type DDContext = DOMContext<Node, Element, Document>;
+export type JSDHyperStatic = HyperStatic<Node, Element>;
+export type JSDContext = DOMContext<Node, Element, Document>;
 
 /**
  * A {@link makeHyper} instance using DenoDOM.
  */
 export const h: HyperStatic<Node, Element> = makeHyper<Node, Element>(
-  new DOMContext<Node, Element, Document>(new Document()),
+  new DOMContext<Node, Element, Document>(new JSDOM("").window.document),
 );
